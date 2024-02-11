fb83abda52216b935c39d84f62472be52998d64c
Stu <99331221+Stu-Air@users.noreply.github.com>
2024-02-11 10:56:45 +0000

    wrong order
    more mistakes -r doesnt exist for mv command, orderwas wrong way .zsh folder is in the mv command.

--- settings.sh
+++ settings.sh
@@ -1,13 +1,7 @@
 #! /bin/bash
 
-#ZSH
-    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions  # Tab completion for suggestions
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.zsh/powerlevel10k
-    chsh -s $(which zsh)
-
-
 #Folders
-    mv -r home/* ~/
+    mv home/* ~/
     ln -sf ~/.minecraft/screenshots ~/Minecraft/screenshots
     ln -sf ~/.minecraft/mods ~/Minecraft/mods
     ln -sf ~/.minecraft/schematics ~/Minecraft/schematics
@@ -15,3 +9,10 @@
     ln -sf ~/.minecraft/saves ~/Minecraft/saves
     ln -sf ~/Pictures/profile-pics/profile.png /var/lib/gdm3/.face
     ln -sf ~/Pictures/profile-pics/profile.png ~/.face 
+
+
+
+#ZSH
+    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions  # Tab completion for suggestions
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.zsh/powerlevel10k
+    chsh -s $(which zsh)
