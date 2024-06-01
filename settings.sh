d4b95ef8f29c5bde53198c9b2ac239f4c7cdf8b7
Stu <99331221+Stu-Air@users.noreply.github.com>
2024-06-01 20:44:55 +0100

    Update settings.sh
    not exiting to continue when combined with ArchInstaller-NG

--- settings.sh
+++ settings.sh
@@ -25,3 +25,5 @@
     git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions  # Tab completion for suggestions
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.zsh/powerlevel10k
     chsh -s $(which zsh)
+
+    exit
